9cfee25ce90046c3261ab40d01ee6b24bba6223d
Nada Elshahawi <76527313+NadaMohamedMedhat@users.noreply.github.com>
2024-04-16 21:08:12 +0200

    run this command flutter packages pub run build_runner watch to built di.config.dart

--- lib/core/DI/di.config.dart
+++ lib/core/DI/di.config.dart
@@ -11,13 +11,20 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../data/datasource_contract/movies_genres_datasource.dart' as _i4;
-import '../../data/datasource_impl/movies_genres_datasource_impl.dart' as _i5;
-import '../../data/repository_impl/movies_genres_repo_impl.dart' as _i7;
-import '../../domain/repository_contract/movies_genres_repo.dart' as _i6;
-import '../../domain/usecases/movies_genres_usecase.dart' as _i8;
+import '../../data/datasource_contract/movies_genres_datasource.dart' as _i6;
+import '../../data/datasource_contract/popular_movies_datasource.dart' as _i4;
+import '../../data/datasource_impl/movies_genres_datasource_impl.dart' as _i7;
+import '../../data/datasource_impl/popular_movies_datasource_impl.dart' as _i5;
+import '../../data/repository_impl/movies_genres_repo_impl.dart' as _i9;
+import '../../data/repository_impl/popular_movies_repo_impl.dart' as _i11;
+import '../../domain/repository_contract/movies_genres_repo.dart' as _i8;
+import '../../domain/repository_contract/popular_movies_repo.dart' as _i10;
+import '../../domain/usecases/movies_genres_usecase.dart' as _i13;
+import '../../domain/usecases/popular_movies_usecase.dart' as _i12;
 import '../../presentation/home/tabs/browse_tab/view_model/browse_tab_view_model.dart'
-    as _i9;
+    as _i15;
+import '../../presentation/home/tabs/home_tab/popular_movies/view_model/popular_movies_view_model.dart'
+    as _i14;
 import '../api/api_manager.dart' as _i3;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -32,14 +39,22 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     gh.singleton<_i3.ApiManager>(() => _i3.ApiManager());
-    gh.factory<_i4.MoviesGenresDataSource>(
-        () => _i5.MoviesGenresDataSourceImpl(gh<_i3.ApiManager>()));
-    gh.factory<_i6.MoviesGenresRepo>(
-        () => _i7.MoviesGenresRepoImpl(gh<_i4.MoviesGenresDataSource>()));
-    gh.factory<_i8.MoviesGenresUseCase>(
-        () => _i8.MoviesGenresUseCase(gh<_i6.MoviesGenresRepo>()));
-    gh.factory<_i9.BrowseTabViewModel>(
-        () => _i9.BrowseTabViewModel(gh<_i8.MoviesGenresUseCase>()));
+    gh.factory<_i4.PopularMoviesDatasource>(
+        () => _i5.PopularMoviesDataSourceImpl(gh<_i3.ApiManager>()));
+    gh.factory<_i6.MoviesGenresDataSource>(
+        () => _i7.MoviesGenresDataSourceImpl(gh<_i3.ApiManager>()));
+    gh.factory<_i8.MoviesGenresRepo>(
+        () => _i9.MoviesGenresRepoImpl(gh<_i6.MoviesGenresDataSource>()));
+    gh.factory<_i10.PopularMoviesRepo>(
+        () => _i11.PopularMoviesRepoImpl(gh<_i4.PopularMoviesDatasource>()));
+    gh.factory<_i12.PopularMoviesUseCase>(
+        () => _i12.PopularMoviesUseCase(gh<_i10.PopularMoviesRepo>()));
+    gh.factory<_i13.MoviesGenresUseCase>(
+        () => _i13.MoviesGenresUseCase(gh<_i8.MoviesGenresRepo>()));
+    gh.factory<_i14.PopularMoviesViewModel>(
+        () => _i14.PopularMoviesViewModel(gh<_i12.PopularMoviesUseCase>()));
+    gh.factory<_i15.BrowseTabViewModel>(
+        () => _i15.BrowseTabViewModel(gh<_i13.MoviesGenresUseCase>()));
     return this;
   }
 }
