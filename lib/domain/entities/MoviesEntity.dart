c28ce1162c08c8c851e1d466bc404c63e9477b0e
Nada Elshahawi <76527313+NadaMohamedMedhat@users.noreply.github.com>
2024-04-17 17:34:00 +0200

    => remove from json & to json functions from MoviesEntity.dart

--- lib/domain/entities/MoviesEntity.dart
+++ lib/domain/entities/MoviesEntity.dart
@@ -30,22 +30,7 @@ class MoviesEntity {
       this.voteAverage, 
       this.voteCount,});
 
-  MoviesEntity.fromJson(dynamic json) {
-    adult = json['adult'];
-    backdropPath = json['backdrop_path'];
-    genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<int>() : [];
-    id = json['id'];
-    originalLanguage = json['original_language'];
-    originalTitle = json['original_title'];
-    overview = json['overview'];
-    popularity = json['popularity'];
-    posterPath = json['poster_path'];
-    releaseDate = json['release_date'];
-    title = json['title'];
-    video = json['video'];
-    voteAverage = json['vote_average'];
-    voteCount = json['vote_count'];
-  }
+
   bool? adult;
   String? backdropPath;
   List<int>? genreIds;
@@ -61,23 +46,6 @@ class MoviesEntity {
   double? voteAverage;
   int? voteCount;
 
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['adult'] = adult;
-    map['backdrop_path'] = backdropPath;
-    map['genre_ids'] = genreIds;
-    map['id'] = id;
-    map['original_language'] = originalLanguage;
-    map['original_title'] = originalTitle;
-    map['overview'] = overview;
-    map['popularity'] = popularity;
-    map['poster_path'] = posterPath;
-    map['release_date'] = releaseDate;
-    map['title'] = title;
-    map['video'] = video;
-    map['vote_average'] = voteAverage;
-    map['vote_count'] = voteCount;
-    return map;
-  }
+
 
 }
